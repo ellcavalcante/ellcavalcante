f473166a0cf58458d46717b01b5c077e7377e00a
Ellington Cavalcante <ecavalcante778@gmail.com>
2022-03-02 20:54:50 -0300

    my first commit :)

---
+++ Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ellington Cavalcante on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
